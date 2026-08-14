82fa8807c4a98c7d1fcc70a5d281a9d6276b41e9
randseas <alchcraft24@gmail.com>
2026-08-14 21:58:02 +0300

    feat: add copyright notice for Material Symbols in lib.rs

--- crates/xengui-icons/src/lib.rs
+++ crates/xengui-icons/src/lib.rs
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 //! Prebuilt material icon widgets.
 
+/// Material Symbols (Copyright Google LLC - Apache 2.0 License)
 pub mod material_symbols;
 pub use material_symbols::*;
